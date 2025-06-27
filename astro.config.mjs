2d155824d4fbb47ea16aa23bb7ab28012c20441a
kelvinComputing <kelvincontrera1@gmail.com>
2025-06-27 16:09:03 -0400

    fix: Guardar cookies al iniciar sesion

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,18 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+	vite: {
+		plugins: [tailwindcss()],
+		server: {
+         host: true, 
+			proxy: {
+				'/api': {
+					target: 'https://livosport.loca.lt',
+					changeOrigin: true, 
+					secure: false, 
+               rewrite: (path) => path.replace(/^\/api/, ''),
+				}
+			}
+		}
+	}
 });
