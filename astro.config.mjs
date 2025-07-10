e0bcda1484c9a87801511b696e18fd3abdb42670
kelvinComputing <kelvincontrera1@gmail.com>
2025-07-10 16:56:59 -0400

    fix: Protegiendo rutas club atleta login

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({
+export default defineConfig({  
+   output: "server",
 	vite: {
 		plugins: [tailwindcss()],
 		server: {
